6d3d75af81baecdc18977a940db70eedde362409
Coronado-JS <jsc28@tamu.edu>
2023-02-12 16:36:19 -0600

    Url Requests

---
+++ CoronadoJTesting.py
@@ -0,0 +1,6 @@
+import requests 
+from urllib.request import urlretrieve  
+
+requests = 'project3logfile.txt'
+#? This is the correct Url request
+url = "https://s3.amazonaws.com/tcmg476/http_access_log"
