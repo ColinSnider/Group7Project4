93530928eea3414a5dca84ec4d214b106a282db8
Sunjeeth Chandramohan <sunjeethc@gmail.com>
2023-02-13 23:46:00 -0600

    fixed #1

--- CoronadoJTesting.py
+++ CoronadoJTesting.py
@@ -1,15 +1,42 @@
-from urllib.request import urlretrieve  
-
-requests = 'project3logfile.txt'
+import requests
+from collections import defaultdict
+import os 
 #? This is the correct Url request
 url = "https://s3.amazonaws.com/tcmg476/http_access_log"
-count = 0
 
-while True: 
-    try:
-        response = requests.get(url)
-        count += 1
-        print(f"Number of request made each day: {count}")
-    except:
-        print("This is the end")
-        break
+
+
+filename = "log_file_local.txt"
+
+def download_local_copy():
+    f=open(filename,"a")
+
+    with requests.get(url,stream=True) as r_open:
+        f.write(r_open.text)
+
+    f.close()
+
+
+if not os.path.isfile(filename):
+    download_local_copy()
+
+
+lines = []
+with open(filename,"r") as f:
+    lines = f.readlines()
+
+
+dct = defaultdict(int)
+
+
+#local - - [24/Oct/1994:13:41:41 -0600] "GET index.html HTTP/1.0" 200 150
+
+'''
+SOME LINES DO NOT HAVE The DATE
+
+'''
+for line in lines:
+    parsed_out_date = line.split(' ')[3].split(':')[0].strip('[]')
+    dct[parsed_out_date]+=1
+
+print(dct)
